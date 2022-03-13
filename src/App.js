361511def208eca5710fc5c89c9ec3fc26363f57
nicole <nicole95082@gmail.com>
2022-03-14 03:17:24 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import axios from 'axios';
+import RepoDetails from './RepoDetails';
 import { useState } from "react";
 
 function App() {
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
   const [repos, setRepos] = useState([]);
+  const [details, setDetails] = useState({})
+  const [detailsLoading, setDetailsLoading] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -24,12 +27,22 @@ function App() {
   }
   function renderRepo(repo) {
     return (
-      <div className='row' key={repo.id}>
+      <div className='row' onClick={() => getDetails(repo.name)} key={repo.id}>
         <h2 className='repo-name'>
           {repo.name}
         </h2>
       </div>
     )
+  }
+  function getDetails(repoName) {
+    setDetailsLoading(true);
+    axios({
+      method: "get",
+      url: 'https://api.github.com/repos/' + username + '/' + repoName,
+    }).then(res => {
+      setDetailsLoading(false);
+      setDetails(res.data);
+    });
   }
   return (
     <div className="page">
@@ -48,6 +61,7 @@ function App() {
             {repos.map(renderRepo)}
           </div>
         </div>
+        <RepoDetails details={details} loading={detailsLoading}/>
       </div>
     </div>
   );
